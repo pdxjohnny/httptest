972dcfa277c902f42934fb9bccd45fd3261ca94e
John Andersen <johnandersenpdx@gmail.com>
2017-08-02 09:04:58 -0700

    Implemented serve_forever shutdown with pipe
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- httptest/httptest.py
+++ httptest/httptest.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 import json
+import socket
+import selectors
+import threading
 import http.server
 import multiprocessing
 
 class FailedToStart(Exception): pass
+
+class AlreadyStarted(Exception): pass
+
+class NotStarted(Exception): pass
 
 class Handler(http.server.SimpleHTTPRequestHandler):
 
@@ -20,10 +27,40 @@ class HTTPServer(http.server.HTTPServer):
 
     allow_reuse_address = True
 
-    def serve_forever(self, *args, **kwargs):
-        self.addr_queue.put(self.server_name)
-        self.addr_queue.put(self.server_port)
-        return super().serve_forever(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__server = False
+        self.__send = False
+
+    def serve_forever(self, addr_queue, pipe):
+        addr_queue.put(self.server_name)
+        addr_queue.put(self.server_port)
+        with selectors.DefaultSelector() as selector:
+            selector.register(self, selectors.EVENT_READ)
+            selector.register(pipe, selectors.EVENT_READ)
+            while True:
+                ready = selector.select()
+                for i in ready:
+                    if i[0].fd != self.fileno():
+                        return self.socket.close()
+                    self._handle_request_noblock()
+
+    def start_background(self):
+        if self.__server != False or self.__send != False:
+            raise AlreadyStarted()
+        addr_queue = multiprocessing.Queue()
+        recv, self.__send = multiprocessing.Pipe()
+        self.__server = threading.Thread(target=self.serve_forever,
+                args=(addr_queue, recv))
+        self.__server.start()
+        return addr_queue.get(True), addr_queue.get(True)
+
+    def stop_background(self):
+        if self.__server == False or self.__send == False:
+            raise NotStarted()
+        self.__send.send('shutdown')
+        self.__server = False
+        self.__send = False
 
 class NoServer(object):
 
@@ -41,15 +78,9 @@ class Server(object):
     def __call__(self, func, *args, **kwargs):
         def wrap(*args, **kwargs):
             s = HTTPServer(self._addr, self._testServerClass)
-            self.__addr_queue = multiprocessing.Queue()
-            s.addr_queue = self.__addr_queue
-            t = multiprocessing.Process(target=s.serve_forever,
-                    daemon=True)
-            t.start()
-            self.server_name = self.__addr_queue.get(True)
-            self.server_port = self.__addr_queue.get(True)
+            self.server_name, self.server_port = s.start_background()
             res = func(*args, ts=self, **kwargs)
-            t.terminate()
+            s.stop_background()
             return res
         return wrap
 
