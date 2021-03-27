d18aa27dd7691179e2e5cc46c2e5c552749f1907
John Andersen <john.s.andersen@intel.com>
2021-03-26 20:15:14 -0700

    setup: Move to PEP-517 style setuptools setup.cfg
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- setup.py
+++ setup.py
@@ -1,47 +1,8 @@
-#!/usr/bin/env python3
-'''
-setup.py: Upload httptest to PyPi
-'''
-import os
-from setuptools import setup, find_packages
+import sys
+import site
+import setuptools
 
-def contents(*a):
-    '''
-    Opens a file relative to this file
-    '''
-    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), *a), 'rb') as filedesc:
-        return filedesc.read().decode('utf-8')
+# See https://github.com/pypa/pip/issues/7953
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 
-# Get the version and README
-VERSION = contents('httptest', 'version.py').split()[-1].replace('\'', '')
-README = contents('README.md')
-
-setup(
-    name='httptest',
-    packages=find_packages(),
-    version=VERSION,
-    data_files=[('', ['LICENSE']), ],
-    description='Add unit tests to your http clients',
-    long_description=README,
-    long_description_content_type='text/markdown',
-    author='John Andersen',
-    author_email='johnandersenpdx@gmail.com',
-    url='https://github.com/pdxjohnny/httptest',
-    download_url='https://github.com/pdxjohnny/httptest/tarball/%s' % VERSION,
-    license='MIT',
-    keywords=['unittesting', 'testing', 'http', 'api', 'test'],
-    classifiers=[
-        'Environment :: Console',
-        'Operating System :: POSIX :: Linux',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Intended Audience :: Developers'
-    ],
-    entry_points={
-        'console_scripts': [
-            'httptest-cache = httptest.cli:cache',
-        ],
-    }
-)
+setuptools.setup()
