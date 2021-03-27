21de77beb7023ccd8e5a08f951ee595a36eda1d1
John Andersen <john.s.andersen@intel.com>
2021-03-26 20:45:38 -0700

    style: Format with black 20.8b1
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- httptest/httptest.py
+++ httptest/httptest.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-'''
+"""
 httptest offers the Handler for serving test data and the HTTPServer
 which is started and stopped using the httptest.Server() decorator.
-'''
+"""
 import os
 import io
 import json
@@ -16,81 +16,90 @@ import multiprocessing
 from urllib.parse import urlparse, urljoin
 from contextlib import contextmanager
 
-if getattr(http.server, 'ThreadingHTTPServer', False):
+if getattr(http.server, "ThreadingHTTPServer", False):
     ThreadingHTTPServer = http.server.ThreadingHTTPServer
 else:
     import socketserver
-    class ThreadingHTTPServer(socketserver.ThreadingMixIn,
-                              http.server.HTTPServer):
+
+    class ThreadingHTTPServer(socketserver.ThreadingMixIn, http.server.HTTPServer):
         pass
+
 
 class FailedToStart(Exception):
-    '''
+    """
     The server failed to start so a NoServer instance was created
-    '''
+    """
+
     pass
+
 
 class AlreadyStarted(Exception):
-    '''
+    """
     The server cannot be started because it is already running
-    '''
+    """
+
     pass
+
 
 class NotStarted(Exception):
-    '''
+    """
     The server cannot be stopped because it isn't running
-    '''
+    """
+
     pass
 
+
 class Handler(http.server.SimpleHTTPRequestHandler):
-    '''
+    """
     Handler to use with httptest.Server
-    '''
+    """
 
     def json(self, data):
-        '''
+        """
         Send a 200 with Content-type application/json using data as
         the json data to send.
-        '''
+        """
         self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+        self.send_header("Content-type", "application/json")
         self.end_headers()
-        self.wfile.write(json.dumps(data).encode('utf-8'))
+        self.wfile.write(json.dumps(data).encode("utf-8"))
 
-    #pylint: disable=arguments-differ
+    # pylint: disable=arguments-differ
     def log_message(self, *args):
-        '''
+        """
         Disables server logs
-        '''
+        """
         pass
 
+
 class CachingProxyHandler(Handler):
-    '''
+    """
     Handler to use with httptest.Server which caches requests to an upstream
     server.
-    '''
+    """
 
     @classmethod
     def to(cls, upstream, state_dir=None):
-        '''
+        """
         Creates a CachingProxyHandler which will proxy requests to an upstream
         server.
-        '''
+        """
         if state_dir is None:
-            state_dir = os.path.join(os.getcwd(), '.cache', 'httptest')
+            state_dir = os.path.join(os.getcwd(), ".cache", "httptest")
 
         upstream = urlparse(upstream)
 
         class ConfiguredCachingProxyHandler(cls):
             UPSTREAM = upstream
             STATE_DIR = state_dir
+
         return ConfiguredCachingProxyHandler
 
     def proxied_url(self):
         url = self.UPSTREAM.geturl() + self.path
-        while '//' in url:
-            url = url.replace('//', '/')
-        return url.replace(':/', '://')
+        while "//" in url:
+            url = url.replace("//", "/")
+        return url.replace(":/", "://")
 
     def cache_path(self, *args):
         if not os.path.isdir(self.STATE_DIR):
@@ -98,25 +107,34 @@ class CachingProxyHandler(Handler):
         return os.path.join(self.STATE_DIR, *args)
 
     def cached(self, key):
-        return bool(all(list(map(lambda needed: \
-                    os.path.isfile(self.cache_path(key + needed)),
-                    ['.status', '.headers', '.body']))))
+        return bool(
+            all(
+                list(
+                    map(
+                        lambda needed: os.path.isfile(self.cache_path(key + needed)),
+                        [".status", ".headers", ".body"],
+                    )
+                )
+            )
+        )
 
     def cache_key(self):
         body = None
-        if 'Content-Length' in self.headers:
-            length = int(self.headers['Content-Length'])
+        if "Content-Length" in self.headers:
+            length = int(self.headers["Content-Length"])
             body = io.BytesIO(self.rfile.read(length))
         digest = hashlib.sha384()
-        digest.update(self.requestline.encode('utf-8', errors='ignore'))
+        digest.update(self.requestline.encode("utf-8", errors="ignore"))
+
         def sort_headers(kv):
-            '''
+            """
             Sort headers dict so it always is in the same order.
-            '''
+            """
             return kv[0].lower()
+
         for k, v in sorted(self.headers.items(), key=sort_headers):
-            digest.update(k.encode('utf-8', errors='ignore'))
-            digest.update(v.encode('utf-8', errors='ignore'))
+            digest.update(k.encode("utf-8", errors="ignore"))
+            digest.update(v.encode("utf-8", errors="ignore"))
         if body is not None:
             digest.update(body.read())
             body.seek(0)
@@ -124,36 +142,36 @@ class CachingProxyHandler(Handler):
 
     @contextmanager
     def save_cache(self, key, status, headers, body):
-        with open(self.cache_path(key + '.hits'), 'w') as fd:
+        with open(self.cache_path(key + ".hits"), "w") as fd:
             fd.write(str(0))
-        with open(self.cache_path(key + '.status'), 'w') as fd:
+        with open(self.cache_path(key + ".status"), "w") as fd:
             fd.write(str(status))
-        with open(self.cache_path(key + '.headers'), 'w') as fd:
+        with open(self.cache_path(key + ".headers"), "w") as fd:
             json.dump(dict(headers._headers), fd)
-        with open(self.cache_path(key + '.body'), 'wb') as fd:
+        with open(self.cache_path(key + ".body"), "wb") as fd:
             fd.write(body.read())
-        with open(self.cache_path(key + '.body'), 'rb') as fd:
+        with open(self.cache_path(key + ".body"), "rb") as fd:
             yield fd
 
     @contextmanager
     def load_cache(self, key):
-        if os.path.exists(self.cache_path(key + '.hits')):
-            with open(self.cache_path(key + '.hits'), 'r') as fd:
+        if os.path.exists(self.cache_path(key + ".hits")):
+            with open(self.cache_path(key + ".hits"), "r") as fd:
                 hits = int(fd.read())
-            with open(self.cache_path(key + '.hits'), 'w') as fd:
+            with open(self.cache_path(key + ".hits"), "w") as fd:
                 fd.write(str(hits + 1))
-        with open(self.cache_path(key + '.status'), 'r') as fd:
+        with open(self.cache_path(key + ".status"), "r") as fd:
             status = int(fd.read())
-        with open(self.cache_path(key + '.headers'), 'r') as fd:
+        with open(self.cache_path(key + ".headers"), "r") as fd:
             headers = json.load(fd)
-        with open(self.cache_path(key + '.body'), 'rb') as fd:
+        with open(self.cache_path(key + ".body"), "rb") as fd:
             yield status, headers, fd
 
     def do_forward(self):
-        '''
+        """
         Forward the request by making a similar request with urllib
-        '''
-        self.headers.replace_header('Host', self.UPSTREAM.netloc)
+        """
+        self.headers.replace_header("Host", self.UPSTREAM.netloc)
         key, data = self.cache_key()
         if self.cached(key):
             # Load from cache
@@ -170,10 +188,9 @@ class CachingProxyHandler(Handler):
                     pass
         else:
             # Run request (not cached)
-            req = urllib.request.Request(self.proxied_url(),
-                                         headers=self.headers,
-                                         data=data,
-                                         method=self.command)
+            req = urllib.request.Request(
+                self.proxied_url(), headers=self.headers, data=data, method=self.command
+            )
             try:
                 with urllib.request.urlopen(req) as f:
                     self.send_response(f.status)
@@ -197,13 +214,14 @@ class CachingProxyHandler(Handler):
 
 
 # Make sure CachingProxyHandler responds to all HTTP methods
-for method in 'GET HEAD POST PUT DELETE CONNECT OPTIONS TRACE PATCH'.split():
-    setattr(CachingProxyHandler, 'do_' + method, CachingProxyHandler.do_forward)
+for method in "GET HEAD POST PUT DELETE CONNECT OPTIONS TRACE PATCH".split():
+    setattr(CachingProxyHandler, "do_" + method, CachingProxyHandler.do_forward)
+
 
 class HTTPServer(ThreadingHTTPServer):
-    '''
+    """
     Starts and manages the running server process.
-    '''
+    """
 
     allow_reuse_address = True
 
@@ -212,11 +230,11 @@ class HTTPServer(ThreadingHTTPServer):
         self.__server = False
         self.__send = False
 
-    #pylint: disable=arguments-differ
+    # pylint: disable=arguments-differ
     def serve_forever(self, addr_queue, pipe):
-        '''
+        """
         Start the server handle requests and wait for shutdown.
-        '''
+        """
         addr_queue.put(self.server_name)
         addr_queue.put(self.server_port)
         with selectors.DefaultSelector() as selector:
@@ -230,48 +248,52 @@ class HTTPServer(ThreadingHTTPServer):
                     self._handle_request_noblock()
 
     def start_background(self):
-        '''
+        """
         Start the server in the background. Call stop_background to
         stop it. Raises AlreadyStarted if called again before
         stop_background is called. Returns the server hostname and
         port as a tuple.
-        '''
+        """
         if self.__server is not False or self.__send is not False:
             raise AlreadyStarted()
         addr_queue = multiprocessing.Queue()
         recv, self.__send = multiprocessing.Pipe()
-        self.__server = threading.Thread(target=self.serve_forever, args=(addr_queue, recv))
+        self.__server = threading.Thread(
+            target=self.serve_forever, args=(addr_queue, recv)
+        )
         self.__server.start()
         return addr_queue.get(True), addr_queue.get(True)
 
     def stop_background(self):
-        '''
+        """
         Stop a running server. Raises NotStarted if called before
         start_background.
-        '''
+        """
         if not self.__server or not self.__send:
             raise NotStarted()
-        self.__send.send('shutdown')
+        self.__send.send("shutdown")
         self.__server = False
         self.__send = False
 
+
 class NoServer(object):
-    '''
+    """
     Used for setting the test server (ts) to a default value for
     the test case.
 
     Example:
         def test_something(self, ts=httptest.NoServer()):
-    '''
+    """
 
     def url(self):
-        '''
+        """
         NoServer always raises FailedToStart on a call to url()
-        '''
+        """
         raise FailedToStart()
 
+
 class Server(object):
-    '''
+    """
     Server is the decorator used on unittest methods.
 
     Example:
@@ -284,26 +306,30 @@ class Server(object):
             def test_json(self, ts=httptest.NoServer()):
                 with urllib.request.urlopen(ts.url()) as f:
                     self.assertEqual(f.read().decode("utf-8"), "[2, 4]")
-    '''
+    """
 
-    def __init__(self, testServerClass, addr=('127.0.0.1', 0)):
+    def __init__(self, testServerClass, addr=("127.0.0.1", 0)):
         self._class = testServerClass
         self._addr = addr
         self.server_name = addr[0]
         self.server_port = addr[1]
 
     def __call__(self, func, *args, **kwargs):
-        '''
+        """
         Starts the HTTPServer runs the test then stops the server
-        '''
+        """
         if inspect.iscoroutinefunction(func):
+
             async def wrap(*args, **kwargs):
                 with self:
                     return await func(*args, ts=self, **kwargs)
+
         else:
+
             def wrap(*args, **kwargs):
                 with self:
                     return func(*args, ts=self, **kwargs)
+
         return wrap
 
     def __enter__(self):
@@ -316,7 +342,7 @@ class Server(object):
         self.server = None
 
     def url(self):
-        '''
+        """
         Server URL formatted as http://server_name:server_port/
-        '''
-        return 'http://{0}:{1}/'.format(self.server_name, self.server_port)
+        """
+        return "http://{0}:{1}/".format(self.server_name, self.server_port)
--- tests/test_httptest.py
+++ tests/test_httptest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-'''
+"""
 Unit tests for httptest
-'''
+"""
 import os
 import glob
 import asyncio
@@ -12,10 +12,11 @@ import urllib.request
 
 import httptest
 
+
 class TestHTTPServer(httptest.Handler):
-    '''
+    """
     Handler for testing httptest.Server
-    '''
+    """
 
     def do_GET(self):
         self.send_response(200)
@@ -23,105 +24,114 @@ class TestHTTPServer(httptest.Handler):
         self.end_headers()
         self.wfile.write(bytes("what up", "utf-8"))
 
+
 class TestServerMethods(unittest.TestCase):
-    '''
+    """
     Test cases for httptest.Server
-    '''
+    """
 
     @httptest.Server(TestHTTPServer)
     def test_url(self, ts=httptest.NoServer()):
-        '''
+        """
         Make sure the Server.url() method works.
-        '''
+        """
         url = ts.url()
-        self.assertIn(':', url)
-        self.assertEqual(':'.join(url.split(':')[:-1]), 'http://localhost')
+        self.assertIn(":", url)
+        self.assertEqual(":".join(url.split(":")[:-1]), "http://localhost")
 
     @httptest.Server(TestHTTPServer)
     def test_call_response(self, ts=httptest.NoServer()):
-        '''
+        """
         Make sure we can read the server's response.
-        '''
+        """
         with urllib.request.urlopen(ts.url()) as f:
-            self.assertEqual(f.read().decode('utf-8'), "what up")
+            self.assertEqual(f.read().decode("utf-8"), "what up")
 
     def test_async_call_response(self):
-        '''
+        """
         Check that httptest.Server works for coroutine functions.
-        '''
+        """
+
         @httptest.Server(TestHTTPServer)
         async def run_test(ts=httptest.NoServer()):
             with urllib.request.urlopen(ts.url()) as f:
-                self.assertEqual(f.read().decode('utf-8'), "what up")
+                self.assertEqual(f.read().decode("utf-8"), "what up")
+
         loop = asyncio.new_event_loop()
         loop.run_until_complete(run_test())
         loop.close()
 
+
 class TestCachingMethods(unittest.TestCase):
-    '''
+    """
     Test cases for httptest.CachingProxyHandler
-    '''
+    """
 
     @httptest.Server(TestHTTPServer)
     def test_forwards_get(self, ts=httptest.NoServer()):
-        '''
+        """
         Make sure we can read the server's response.
-        '''
+        """
         with tempfile.TemporaryDirectory() as tempdir:
-            @httptest.Server(httptest.CachingProxyHandler.to(ts.url(),
-                             state_dir=tempdir))
+
+            @httptest.Server(
+                httptest.CachingProxyHandler.to(ts.url(), state_dir=tempdir)
+            )
             def test_cached(ts=httptest.NoServer()):
-                with urllib.request.urlopen(ts.url() + 'get') as f:
-                    self.assertEqual(f.read().decode('utf-8'), "what up")
-                self.assertEqual(len(list(glob.glob(os.path.join(tempdir,
-                    '*')))), 4)
-                with open(glob.glob(os.path.join(tempdir, '*.body'))[0], 'wb') \
-                        as fd:
+                with urllib.request.urlopen(ts.url() + "get") as f:
+                    self.assertEqual(f.read().decode("utf-8"), "what up")
+                self.assertEqual(len(list(glob.glob(os.path.join(tempdir, "*")))), 4)
+                with open(glob.glob(os.path.join(tempdir, "*.body"))[0], "wb") as fd:
                     fd.write(b"waassss aaaap")
-                with urllib.request.urlopen(ts.url() + 'get') as f:
-                    self.assertEqual(f.read().decode('utf-8'), "waassss aaaap")
+                with urllib.request.urlopen(ts.url() + "get") as f:
+                    self.assertEqual(f.read().decode("utf-8"), "waassss aaaap")
 
             test_cached()
 
     @httptest.Server(TestHTTPServer)
     def test_forwards_error(self, ts=httptest.NoServer()):
-        '''
+        """
         Make sure we can read the server's response.
-        '''
+        """
         with tempfile.TemporaryDirectory() as tempdir:
-            @httptest.Server(httptest.CachingProxyHandler.to(ts.url(),
-                             state_dir=tempdir))
+
+            @httptest.Server(
+                httptest.CachingProxyHandler.to(ts.url(), state_dir=tempdir)
+            )
             def test_cached(ts=httptest.NoServer()):
-                req = urllib.request.Request(ts.url() + 'p', method='POST')
+                req = urllib.request.Request(ts.url() + "p", method="POST")
                 with self.assertRaises(urllib.error.HTTPError):
                     with urllib.request.urlopen(req) as f:
                         pass
 
             test_cached()
 
+
 class TestJSONServer(httptest.Handler):
-    '''
+    """
     Handler for testing httptest.Handler
-    '''
+    """
 
     def do_GET(self):
-        '''
+        """
         Returns [2, 4]
-        '''
+        """
         self.json([2, 4])
 
+
 class TestHandlerMethods(unittest.TestCase):
-    '''
+    """
     Test cases for httptest.Handler
-    '''
+    """
 
     @httptest.Server(TestJSONServer)
     def test_json(self, ts=httptest.NoServer()):
-        '''
+        """
         Make sure the server correctly encodes and sends us a json.
-        '''
+        """
         with urllib.request.urlopen(ts.url()) as f:
-            self.assertEqual(f.read().decode('utf-8'), "[2, 4]")
+            self.assertEqual(f.read().decode("utf-8"), "[2, 4]")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
